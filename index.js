507e351f96c430f3eee4360e52c25f1ce5ae9a61
luxkaywalker <lucascaio.sena@gmail.com>
2016-03-05 10:33:46 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+server.use(jsonServer.defaults());
+
+var router = jsonServer.router('db.json');
+
+
+server.use(router);
+server.listen(process.env.PORT || 5000);
